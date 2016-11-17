8879f885029efbd5a3b46a3c6e6b2e0d75940cee
Joram Kimata <joramkimata@gmail.com>
2016-11-17 18:03:53 +0300

    initialize the repo

---
+++ src/L5UtilsHelperServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Izweb\L5UtilsHelper;
+
+use Illuminate\Support\ServiceProvider;
+
+class L5UtilsHelperServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
